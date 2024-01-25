2dc6ab3ca00449bd833cb5bfc79701cb2db4b293
TanayDesai <tanaydesai5.com>
2024-01-25 21:35:50 +0530

    Updated

--- app/impulsive/page.js
+++ app/impulsive/page.js
@@ -7,8 +7,8 @@ const Home = () => {
         <div className="content">
             <div className="content-box bg-transparent pt-[80px] shadow-none"><Implusive /></div>
 
-            <div className="content-blog">
-                <h1 className="content-header">Implusive</h1>
+            {/* <div className="content-blog">
+                <h1 className="content-header">Impulsive</h1>
                 <div className="content-description">
                     <p>A boucy, fluid, speedy & hoverable component for displaying links or socials.</p>
                     <p>Try hovering over the links:)</p>
@@ -22,7 +22,7 @@ const Home = () => {
                     <li><code >tailwindcss</code></li>
                     <p>inspired by the legendary designer <a href="https://twitter.com/laurentdelrey/status/1712467248768749789" className="text-black link">@saintlaurendelrey</a>'s "ship to friends" concept.</p>
                 </div>   
-            </div> 
+            </div>  */}
         </div>
     )
 }
--- app/layout.js
+++ app/layout.js
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        {/* <NavBar /> */}
+        <NavBar />
         {children}
         <hr className='divder'></hr>
         <Footer />
